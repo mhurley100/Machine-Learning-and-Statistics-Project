45de939f44267ee4eeb65458e85e8182ac7989e2
Maura Hurley <G00376337@gmit.ie>
2021-01-04 08:12:35 +0000

    Finalising notebook and commencing work on server

--- server.py
+++ server.py
@@ -2,6 +2,8 @@
 import flask as fl
 # numpy for numerical work.
 import numpy as np
+import json
+from randF import RandomF as rf
 
 # Create a new web app.
 app = fl.Flask(__name__)
@@ -11,10 +13,12 @@ app = fl.Flask(__name__)
 def home():
   return app.send_static_file('index.html')
 
-# Add uniform route.
-@app.route('/api/uniform')
-def uniform():
-  return {"value": np.random.uniform()}
+# Add linear route.
+@app.route('/api/randF/<string:speed>')
+def RF(speed):
+  speed = str(speed)
+  return {"value":rf.randF(speed)}
+
 
 # Add normal route.
 @app.route('/api/normal')
